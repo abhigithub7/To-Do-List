ce3f15d7b2477e443aaed3a80d293a856c85b58c
Abhinay Ahirwar <you@example.com>
2025-11-01 13:42:59 +0530

    Switch to PostgreSQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,11 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgresql://users_eit4_user:x52HDnBvPVSSBCxFrE1WFuOl9Yqa6GmS@dpg-d42rvr9r0fns739ll460-a/users_eit4')
+}
 
 
 # Quick-start development settings - unsuitable for production
